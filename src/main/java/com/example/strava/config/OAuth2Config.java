4d9202c398cee7cd7329386cde34871625aa3f8f
Arun Gupta <arun.gupta@gmail.com>
2025-11-16 20:30:49 +0100

    Revert OAuth error handler: use DefaultResponseErrorHandler to avoid Strava API parsing issues

--- src/main/java/com/example/strava/config/OAuth2Config.java
+++ src/main/java/com/example/strava/config/OAuth2Config.java
@@ -23,8 +23,8 @@ public class OAuth2Config {
                 new FormHttpMessageConverter(),
                 new OAuth2AccessTokenResponseHttpMessageConverter()));
 
-        // Use OAuth2 error handler to properly handle OAuth2 error responses
-        restTemplate.setErrorHandler(new OAuth2ErrorResponseErrorHandler());
+        // Use default error handler to avoid OAuth2 error format parsing issues
+        restTemplate.setErrorHandler(new org.springframework.web.client.DefaultResponseErrorHandler());
 
         client.setRestOperations(restTemplate);
         return client;
